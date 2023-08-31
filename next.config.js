d390b3e1af2021d3be0835d916ebffbc2ae5a871
LEE-YO-HAN <yhl0078@gmail.com>
2023-08-31 23:36:27 +0900

    styledComponents setup with SSR

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  compiler: {
+    styledComponents: {
+      ssr: true,
+    },
+  },
 };
 
 module.exports = nextConfig;
