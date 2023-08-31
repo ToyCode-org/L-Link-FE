162ba4377ed662ad6f739e7f138d626324a5b825
LEE-YO-HAN <yhl0078@gmail.com>
2023-08-31 21:25:42 +0900

    husky test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={stylesqvdwv.main}>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
