d009d818dfb5ce067c5e9eff0480e49da150ef2d
LEE-YO-HAN <yhl0078@gmail.com>
2023-09-05 19:34:08 +0900

    layout test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Header } from "@/components/layout/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LOA-Link",
   description: "---",
-  keywords: "",
+  keywords: "---",
 };
 
 export default function RootLayout({
@@ -17,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Header />
+      </body>
     </html>
   );
 }
